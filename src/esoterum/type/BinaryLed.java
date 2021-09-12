72294b562d0518f5912ff94691cf1ccae0244526
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-09-12 13:43:22 -0300

    change strength of leds colors (#12)
    monkae

--- src/esoterum/type/BinaryLed.java
+++ src/esoterum/type/BinaryLed.java
@@ -4,8 +4,14 @@ import arc.Core;
 import arc.graphics.Color;
 import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.TextureRegion;
+import arc.scene.ui.layout.*;
 import arc.util.*;
+import arc.util.io.*;
+import arc.struct.*;
 import mindustry.graphics.*;
+import mindustry.gen.*;
+import mindustry.input.*;
+import static mindustry.Vars.*;
 
 public class BinaryLed extends BinaryAcceptor {
     public boolean[] inputs = {true, true, true};
@@ -15,6 +21,9 @@ public class BinaryLed extends BinaryAcceptor {
         drawArrow = false;
         emits = false;
         drawConnection = false;
+        configurable = true;
+
+        config(Integer.class, (BinaryLedBuild tile, Integer value) -> tile.color = new Color(value));
     }
 
     @Override
@@ -27,6 +36,15 @@ public class BinaryLed extends BinaryAcceptor {
     }
 
     public class BinaryLedBuild extends BinaryAcceptorBuild {
+        public Color color = Color.white;
+
+        @Override
+        public void buildConfiguration(Table table){
+            table.button(Icon.pencil, () -> {
+                ui.picker.show(Tmp.c1.set(color), false, res -> configure(res.rgba()));
+                deselect();
+            }).size(40f);
+        }
 
         @Override
         public void updateTile(){
@@ -43,8 +61,26 @@ public class BinaryLed extends BinaryAcceptor {
         @Override
         public void draw(){
             Draw.rect(region, x, y);
-            Draw.color(Tmp.c1.set(getSignal(nb[1]) ? 1f : 0f, getSignal(nb[0]) ? 1f : 0f, getSignal(nb[2]) ? 1f : 0f));
+            Draw.color(new Color(getSignal(nb[0]) ? color.r : 0f, getSignal(nb[1]) ? color.g : 0f, getSignal(nb[2]) ? color.b : 0f));
             Draw.rect(topRegion, x, y, rotdeg());
         }
+
+        @Override
+        public Number config(){
+            return color.rgba();
+        }
+
+        @Override
+        public void write(Writes write){
+            super.write(write);
+            write.i(color.rgba());
+        }
+
+        @Override
+        public void read(Reads read, byte revision){
+            super.read(read, revision);
+            color = new Color(read.i());
+        }
+
     }
 }
