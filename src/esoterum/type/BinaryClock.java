f692bfc7b1844278cb7d75c2130fc6704d10be77
MEEP of Faith <meepmatthew@gmail.com>
2021-09-14 22:51:29 -0700

    Seconds based Binary Clock
    Also yes, text box

--- src/esoterum/type/BinaryClock.java
+++ src/esoterum/type/BinaryClock.java
@@ -1,6 +1,8 @@
 package esoterum.type;
 
 import arc.math.*;
+import arc.scene.*;
+import arc.scene.ui.*;
 import arc.scene.ui.layout.Table;
 import arc.util.*;
 import arc.util.io.Reads;
@@ -11,16 +13,16 @@ import mindustry.ui.Styles;
 public class BinaryClock extends BinarySwitch {
     public BinaryClock(String name){
         super(name);
-        config(Float.class, (BinaryClockBuild tile, Float frequency) -> tile.frequency = frequency);
+        config(Float.class, (BinaryClockBuild tile, Float interval) -> tile.interval = interval);
     }
 
     public class BinaryClockBuild extends BinarySwitchBuild {
-        public float frequency = 1f;
+        public float interval = 1f; //In seconds
 
         @Override
         public void updateTile(){
             lastSignal = nextSignal;
-            nextSignal = Mathf.sin(Time.time * (Mathf.PI2 / (60 / frequency))) >= 0f;
+            nextSignal = Mathf.sin(Time.time * (Mathf.PI2 / (60 * interval))) >= 0f;
         }
 
         @Override
@@ -30,7 +32,7 @@ public class BinaryClock extends BinarySwitch {
 
         @Override
         public Object config(){
-            return frequency;
+            return interval;
         }
 
         @Override
@@ -42,21 +44,29 @@ public class BinaryClock extends BinarySwitch {
         public void buildConfiguration(Table table) {
             table.setBackground(Styles.black5);
             table.button("-", () -> {
-                frequency -= 0.1f;
-                frequency = Math.round(frequency * 10) / 10f;
+                interval -= 0.1f;
+                interval = Math.round(interval * 10) / 10f;
 
-                if(frequency < 0.1f) frequency = 0.1f;
+                if(interval < 0.1f) interval = 0.1f;
             }).size(40);
-            table.label(() -> frequency + "hz").labelAlign(Align.center)
-                    .growX()
-                    .fillX()
-                    .center()
-                    .size(80, 40);
+            TextField iField = table.field(interval + "s", s -> {
+                    if(!s.isEmpty()){
+                        s = s.replaceAll("[^\\d.]", ""); //God, I love google. I have no idea what the first part even is.
+                        interval = Float.parseFloat(s);
+                    }
+                }).labelAlign(Align.center)
+                .growX()
+                .fillX()
+                .center()
+                .size(80, 40)
+                .get();
+            iField.update(() -> {
+                Scene stage = iField.getScene();
+                if(!(stage != null && stage.getKeyboardFocus() == iField)) iField.setText(interval + "s");
+            });
             table.button("+", () -> {
-                frequency += 0.1f;
-                frequency = Math.round(frequency * 10) / 10f;
-
-                if(frequency > 69f) frequency = 69f;
+                interval += 0.1f;
+                interval = Math.round(interval * 10) / 10f;
             }).size(40);
         }
 
@@ -71,7 +81,7 @@ public class BinaryClock extends BinarySwitch {
 
             if(revision == 1){
                 nextSignal = lastSignal = read.bool();
-                frequency = read.f();
+                interval = read.f();
             }
         }
 
@@ -80,7 +90,7 @@ public class BinaryClock extends BinarySwitch {
             super.write(write);
 
             write.bool(lastSignal);
-            write.f(frequency);
+            write.f(interval);
         }
     }
 }
